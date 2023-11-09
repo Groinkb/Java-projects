b4e2fd8cd5502e89460383ea2fbd0fbebb5487be
Groinkb <benjamin@mossu.ch>
2023-11-09 13:29:33 +0100

    java

--- Snake/src/App.java
+++ Snake/src/App.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+
+	public static void main(String[] args) throws Exception{
+		   int boardWidth = 800;
+			 int boardHeight = boardWidth;
+
+			 JFrame frame = new JFrame("Snake");
+			 frame.setVisible(true);
+			 frame.setSize(boardWidth, boardHeight);
+			 frame.setLocationRelativeTo(null);
+			 frame.setResizable(false);
+			 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);}
+	}
